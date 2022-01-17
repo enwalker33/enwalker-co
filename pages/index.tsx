d5bb0fe583e476528f499b9154916f8672161332
Emmanuel Walker <Walkeremmanuel33@gmail.com>
2022-01-17 08:15:44 -0700

    added component folders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head'
+import autumn from './public/autumnforest.jpg'
+
 
 export default function Home() {
       return (
         <div>
+          <Head>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
@@ -10,8 +13,9 @@ export default function Home() {
           <meta name="description" content="" />
           <meta name="keywords" content="" />
           <meta name="author" content="" />
-        
-          <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" /> 
+          <link rel="stylesheet" href="./dist/tailwind.css" /> 
+          </Head>
+ 
           <div className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
       {/*TODO: add header component*/}
         
@@ -19,7 +23,7 @@ export default function Home() {
             <div id="profile" className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
               <div className="p-4 md:p-12 text-center lg:text-left">
                 {/* Image for mobile view*/}
-                <div className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style={{backgroundImage: 'url(public/EW2020.JPG)'}} />
+                <div className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style={{backgroundImage: 'url(${autumn})'}} />
                 <h1 className="text-3xl font-bold pt-8 lg:pt-0">Emmanuel Nabge Walker </h1>
                 <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25" />
                 <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg className="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" /></svg> JR Javascript Developer</p>
